18bffa2445c3f90edd5fd0456d887f5e58b6b9ef
Kit <kit@FossilBookAir.local>
2023-06-21 14:02:13 +1200

    add back button

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ struct Narwhal {
 #[derive(Debug, Clone)]
 enum Message {
     FileClicked(usize),
+    GoBack,
 }
 
 impl Default for Narwhal {
@@ -75,11 +76,22 @@ impl Application for Narwhal {
                     self.files.push(path.unwrap())
                 }
             },
+            Message::GoBack => {
+                self.currentpath.pop();
+                self.files = vec![];
+                let read_output = match fs::read_dir(self.currentpath.clone()) {
+                    Ok(x) => x,
+                    Err(y) => panic!("{}", y),
+                };
+                for path in read_output {
+                    self.files.push(path.unwrap())
+                }
+            }
         }
         iced::Command::none()
     }
     fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        let btn_test = Button::new("test");
+        let back_btn = Button::new("Back").on_press(Message::GoBack);
         let mut file_listing = Column::new();
         for i in 0..self.files.len() {
             let filename = self.files[i].file_name();
@@ -88,7 +100,7 @@ impl Application for Narwhal {
             let button = Button::new(text).on_press(Message::FileClicked(i));
             file_listing = file_listing.push(button)
         }
-        let row_test = Row::new().push(btn_test).push(file_listing);
+        let row_test = Row::new().push(back_btn).push(file_listing);
         let column_test = Column::new().push(row_test);
         Container::new(column_test).width(Length::Fill).height(Length::Fill).into()
     }
